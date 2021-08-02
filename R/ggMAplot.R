203c1c66aac194a44ab8fac21d284cfdc739c1aa
ATpoint <a.toenges@uni-muenster.de>
2021-08-02 14:45:12 +0200

    fixed some minor bugs related to axis limits

--- R/ggMAplot.R
+++ R/ggMAplot.R
@@ -21,8 +21,8 @@
 #' @param NonSig legend label for points being "NonSig"
 #' @param point.size size of data points
 #' @param point.alpha point opacity
-#' @param xlab x label
-#' @param ylab y label
+#' @param xlab x-axis label, if NULl then defaults to "baseMean" for MA-plots and "log2FoldChange" for Volcanos
+#' @param ylab y-axis label, if NULl then defaults to "log2FoldChange" for MA-plots and "-log10(padj)" for Volcanos
 #' @param xlim xaxis limits, by default full data range
 #' @param ylim yaxis limits, by default the 0.001th and 0.999th quantile of the data range, see details
 #' @param title main title text
@@ -42,6 +42,8 @@
 #' @param no.legend logical, whether to remove the legend
 #' @param legend.position legend positon, one of left/right/top/bottom 
 #' @param legend.addnumbers logical, whether to add the number of Up/Down/NonSig genes to the legend
+#' @param return.data logical, whether to return the long-form data.frame that was sed for plotting.
+#' In this case the output is a list with elements "plot" and "data".
 #' 
 #' @author Alexander Toenges
 #' 
@@ -63,7 +65,8 @@
 #' Points beyond these limits will be trimmed back to these exact limits and displayed as trianges rather
 #' than dots. The user can set custom quantiles or simply use an explicit \code{xlim} or \code{ylim}
 #' value to manually set axis limits. The automatic axis limit setting can be turned off by setting
-#' \code{quantiles.x} or \code{quantiles.y} to NULL or \code{c(0,1)}.
+#' \code{quantiles.x} or \code{quantiles.y} t0 \code{c(0,1)}. If \code{xlim} or \code{ylim} are provided then
+#' any quantile parameters will be overwritten.
 #' 
 #' => towards labels \cr 
 #' The user can provide a vector of gene names via \code{labels} which is then internally added to the ggplot
@@ -77,47 +80,48 @@
 #' dds <- DESeq2::DESeq(DESeq2::makeExampleDESeqDataSet(5000,10))
 #' res <- DESeq2::results(dds) %>% data.frame %>% na.omit
 #' res$baseMean<-log2(res$baseMean+1)
+#' res$padj <- res$pvalue
 #' theme_set(theme_classic(base_size = 12.5))
 #' 
-#' # a standard MA-plot (pvalue instead of padj simply because then we get more genes in color)
-#' ggMAplot(xval = res$baseMean, yval = res$log2FoldChange, pval = res$pvalue,
-#' title = "DE results", subtitle = "most basic type of MA-plot")
+#' # a standard MA-plot 
+#' ggMAplot(xval = res$baseMean, yval = res$log2FoldChange, pval = res$padj,
+#' title = "MA-plot", subtitle = "most basic version")
 #' 
 #' # with an abline at abs(log2(1.5)) and at 0:
-#' ggMAplot(xval = res$baseMean, yval = res$log2FoldChange, pval = res$pvalue,
-#' y.ablines = c(log2(1.5),-log2(1.5)), title = "DE results", subtitle = "with ablines")
+#' ggMAplot(xval = res$baseMean, yval = res$log2FoldChange, pval = res$padj,
+#' y.ablines = c(log2(1.5),-log2(1.5)), title = "MA-plot", subtitle = "with some ablines")
 #' 
 #' #/ show only from -2 to 2 on the y-axis, points beyond the limits will be printed as triangles:
-#' ggMAplot(xval = res$baseMean, yval = res$log2FoldChange, pval = res$pvalue,
-#' y.ablines = c(log2(1.5),-log2(1.5)), title = "DE results", subtitle = "with ablines",
+#' ggMAplot(xval = res$baseMean, yval = res$log2FoldChange, pval = res$padj,
+#' y.ablines = c(log2(1.5),-log2(1.5)), title = "MA-plot", subtitle = "with ablines and restricted at the y-axis to -2/+2",
 #' ylim=c(-2,2))
 #' 
 #' # as Volcano:
-#' ggMAplot(xval = res$log2FoldChange, yval = -log10(res$pvalue), pval = res$pvalue,
-#' title = "DE results", subtitle = "Volcano plot", preset = "volcano")
+#' ggMAplot(xval = res$log2FoldChange, yval = -log10(res$padj), pval = res$padj,
+#' title = "Volcano", subtitle = "most basic version", preset = "volcano")
 #' 
 #' # Volcano with only abs(logFC) > 2 colored
-#' ggMAplot(xval = res$log2FoldChange, yval = -log10(res$pvalue), pval = res$pvalue,
-#' title = "DE results", subtitle = "Volcano plot", preset = "volcano", xval.thresh=2,
+#' ggMAplot(xval = res$log2FoldChange, yval = -log10(res$padj), pval = res$padj,
+#' title = "Volcano", subtitle = "with some ablines", preset = "volcano", xval.thresh=2,
 #' x.ablines=c(-2,2))
 #' 
-#' # Label significant regions, threshold pvalue<0.001 & abs(log2FoldChange) > log2(2).
+#' # Label significant regions, threshold padj<0.001 & abs(log2FoldChange) > log2(2).
 #' # Note that for the \code{labels} option we make a custom vector, same order as for the
 #' # xval/yval/pval vectors, setting those genes we do **not** want to plot as \code{""}.
 #' # It is now on the user to tweak the number of genes to label and to play with \code{max.overlaps}
 #' # in \code{geom_text_repel} until the plot looks satisfying.
 #' library(ggrepel)
-#' ggMAplot(xval = res$log2FoldChange, yval = -log10(res$pvalue), pval = res$pvalue,
-#' labels=ifelse(res$pvalue < 0.001 & abs(res$log2FoldChange) > log2(2), rownames(res), ""),
-#' title = "DE results", subtitle = "Volcano plot", preset = "volcano", xval.thresh=log2(2)) +
+#' ggMAplot(xval = res$log2FoldChange, yval = -log10(res$padj), pval = res$padj,
+#' labels=ifelse(res$padj < 0.001 & abs(res$log2FoldChange) > log2(2), rownames(res), ""),
+#' title = "Volcano", subtitle = "with padj < 0.001 labelled", preset = "volcano", xval.thresh=log2(2)) +
 #' geom_text_repel(aes(label=labels), show.legend=FALSE, max.overlaps=20, min.segment.length=0)
 #' 
-#' # or only gene number 500:
+#' # or only gene number 1, see also https://ggrepel.slowkow.com/articles/examples.html for many options to customize ggrepel:
 #' library(ggrepel)
-#' ggMAplot(xval = res$log2FoldChange, yval = -log10(res$pvalue), pval = res$pvalue,
-#' labels=ifelse(rownames(res) == "gene500", rownames(res), ""),
-#' title = "DE results", subtitle = "Volcano plot", preset = "volcano", xval.thresh=log2(2)) +
-#' geom_text_repel(aes(label=labels), show.legend=FALSE, max.overlaps=Inf, min.segment.length=0)
+#' ggMAplot(xval = res$log2FoldChange, yval = -log10(res$padj), pval = res$padj,
+#' labels=ifelse(rownames(res) == "gene1", rownames(res), ""),
+#' title = "Volcano", subtitle = "with specific genes labelled", preset = "volcano", xval.thresh=log2(2)) +
+#' geom_text_repel(aes(label=labels), show.legend=FALSE, max.overlaps=Inf, nudge_y=0.5)
 #' 
 #' 
 #' @export
@@ -126,14 +130,17 @@ ggMAplot  <- function(xval, yval, pval=NULL, labels=NULL,
                       col.base="grey50", col.up="firebrick", col.down="darkblue",
                       Up="Up", Down="Down", NonSig="NonSig",
                       point.size=1, point.alpha=0.75,
-                      xlab="baseMean", ylab="logFC",
+                      xlab=NULL,
+                      ylab=NULL,
                       xlim=NULL, ylim=NULL,
                       title=waiver(), subtitle=waiver(),
                       x.ablines=NULL, x.ablines.col="black", x.ablines.lty="dashed",
                       y.ablines=NULL, y.ablines.col="black", y.ablines.lty="dashed",
-                      quantiles.x=c(0, 1), quantiles.y=c(0.001, 0.999),
-                      preset=c("maplot", "volcano"), no.legend=FALSE, 
-                      legend.position="bottom", legend.addnumbers=TRUE)
+                      quantiles.x=c(0, 1),
+                      quantiles.y=c(.001, .999),
+                      preset=c("maplot", "volcano"),
+                      no.legend=FALSE, legend.position="bottom", legend.addnumbers=TRUE,
+                      return.data=FALSE)
 {
   
   #----------------------------------
@@ -143,52 +150,44 @@ ggMAplot  <- function(xval, yval, pval=NULL, labels=NULL,
   invisible(match.arg(class(xval.thresh), c("numeric", "NULL")))
   invisible(match.arg(class(yval.thresh), c("numeric", "NULL")))
   invisible(match.arg(class(pval.thresh), c("numeric", "NULL")))
-  invisible(match.arg(preset, c("maplot", "volcano")))
   invisible(match.arg(legend.position, c("left", "right", "top", "bottom")))
-  preset <- match.arg(preset)
-  
+  invisible(match.arg(preset, c("maplot", "volcano"))); preset <- match.arg(preset)
+
+  #----------------------------------
+  #/ if no labels add NAs:
   if(!is.null(labels)) {
     if(length(labels)!=length(xval)){
       stop("[Error] Length of label must be the same as of xval/yval/pval")
     }
   } else labels <- rep(NA, length(xval))
   
-  #/ maplot preset is just the default but volcano is not:
-  if(preset=="volcano"){
-    if(is.null(xval.thresh)) xval.thresh <- 0
-    if(yval.thresh==0) yval.thresh <- NULL
-    if(xlab=="baseMean") xlab <- "logFC"
-    if(ylab=="logFC") ylab <- "-log10(FDR)"
-    if(all(quantiles.x==c(0,1))) quantiles.x <- c(0.001, 0.999)
-    if(all(quantiles.y==c(0.001,0.999))) quantiles.y <- c(0,1)
-  }
-  
   #----------------------------------
-  # axis limits depending on the
-  # xval and yval thresholds and
-  # quantiles
-  
+  # axis limits depending on the xval/yval thresholds and quantiles
   if(is.null(xlim)){
-    xlim <- as.numeric(quantile(xval, quantiles.x))
+    if(all(quantiles.x == c(0,1))) {
+      xlim <- c(min(xval), max(xval))
+    } else xlim <- as.numeric(quantile(xval, quantiles.x))
   }
   
   if(is.null(ylim)){
-    ylim <- as.numeric(quantile(yval, quantiles.y))
+    if(all(quantiles.y == c(0,1))) {
+      ylim <- c(min(yval), max(yval))
+    } else ylim <- as.numeric(quantile(yval, quantiles.y))
   }
   
   #----------------------------------
-  # mutate the data into final form
+  # mutate the data into final long form
   df <- data.frame(xval, yval, labels) %>%
     
     #/ add p-values
     mutate(pval=case_when(is.null(pval) ~ rep(1, length(xval)), TRUE ~ pval)) %>%
     
     #/ if beyond xlim/ylim trim to specified x/ylim and plot as triangle:
-    mutate(out1=case_when(xval >= xlim[2] ~ "yes_x_top",
-                          xval <= xlim[1] ~ "yes_x_bottom",
+    mutate(out1=case_when(xval > xlim[2] ~ "yes_x_top",
+                          xval < xlim[1] ~ "yes_x_bottom",
                           TRUE ~ "no"),
-           out2=case_when(yval >= ylim[2] ~ "yes_y_top",
-                          yval <= ylim[1] ~ "yes_y_bottom",
+           out2=case_when(yval > ylim[2] ~ "yes_y_top",
+                          yval < ylim[1] ~ "yes_y_bottom",
                           TRUE ~ "no")) %>%
     
     mutate(xval=case_when(out1 == "yes_x_top"    ~ xlim[2],
@@ -216,6 +215,9 @@ ggMAplot  <- function(xval, yval, pval=NULL, labels=NULL,
       yval.thresh <- 0
     }
     
+    if(is.null(xlab)) xlab <- "baseMean"
+    if(is.null(ylab)) ylab <- "log2FoldChange"
+    
     df <- df %>%
       mutate(signif=factor(case_when(
       pval < pval.thresh & xval > xval.thresh & yval > yval.thresh ~ get(Up),
@@ -228,6 +230,9 @@ ggMAplot  <- function(xval, yval, pval=NULL, labels=NULL,
     if(is.null(xval.thresh)){
       xval.thresh <- 0
     }
+    
+    if(is.null(xlab)) xlab <- "log2FoldChange"
+    if(is.null(ylab)) ylab <- "-log10(padj)"
     
     df <- df %>%
       mutate(signif=factor(case_when(
@@ -289,6 +294,8 @@ ggMAplot  <- function(xval, yval, pval=NULL, labels=NULL,
   #/ if no legend:
   if(no.legend) gg <- gg + theme(legend.position="none")
   
-  return(gg)
+  if(!return.data) {
+    return(gg) 
+  } else return(list(plot=gg, data=df))
   
 }
